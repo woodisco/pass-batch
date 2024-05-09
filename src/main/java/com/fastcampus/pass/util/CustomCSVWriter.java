7ca6d42833e5b7025f9c0d53f9bc2be9179149a7
woodisco <woodisco.official@gmail.com>
2024-05-09 11:31:48 +0900

    CustomCSVWriter 수정

--- src/main/java/com/fastcampus/pass/util/CustomCSVWriter.java
+++ src/main/java/com/fastcampus/pass/util/CustomCSVWriter.java
@@ -1,8 +1,8 @@
 package com.fastcampus.pass.util;
 
+import com.opencsv.CSVWriter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.util.List;
 
@@ -10,16 +10,14 @@ import java.util.List;
 public class CustomCSVWriter {
     public static int write(final String fileName, List<String[]> data) {
         int rows = 0;
-        try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
-            bw.write(data.toString());
+        try (CSVWriter writer = new CSVWriter(new FileWriter(fileName))) {
+            writer.writeAll(data);
             rows = data.size();
-            bw.close();
         } catch (Exception e) {
             log.error("CustomCSVWriter - write: CSV 파일 생성 실패, fileName: {}", fileName);
 
         }
-
         return rows;
+
     }
 }
